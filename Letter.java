2932d069115df090893c738fb1c43933d3c054ef
John Khalife <jpa.khalife@gmail.com>
2024-10-08 15:53:05 -0400

    Created the letter class

--- Letter.java
+++ Letter.java
@@ -1,3 +1,25 @@
+/**
+ * The Letter class is responsible for tracking the state of scrabble
+ * and contains the main line of execution
+ * @author Elyssa Grant, Gillian O'Connel, John Khalife, Sandy Alzabadani 
+ * @date 08/10/2024
+ */
+
 public class Letter {
-    
+
+    //Member variables
+    private char letter;
+    private int points;
+
+    public Letter(char letter, char points) {
+        this.letter = letter;
+        this.points = points;
+    }
+    public char getLetter() {
+        return letter;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 }
