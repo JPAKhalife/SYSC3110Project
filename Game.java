f2f4c23adc6667b12c2ebe3228dc4aad145e72cc
John Khalife <jpa.khalife@gmail.com>
2024-10-08 15:42:09 -0400

    Created initial class files

---
+++ Game.java
@@ -0,0 +1,22 @@
+/**
+ * The game class is responsible for tracking the state of scrabble
+ * and contains the main line of execution
+ * @author Elyssa Grant, Gillian O'Connel, John Khalife, Sandy Alzabadani 
+ * @date 08/10/2024
+ */
+
+enum Words {
+    // THIS {,
+    // IS("is"),
+    // A,
+    // PLACE,
+    // TO,
+    // KEEP,
+    // WORDS
+} 
+
+public class Game {
+
+    
+
+}
