0038e7bf52171f45a5ab8ba9425dead667840711
John Khalife <jpa.khalife@gmail.com>
2024-10-08 16:44:37 -0400

    - some temporary changes

---
+++ Words.java
@@ -0,0 +1,24 @@
+/**
+ * The game class is responsible for tracking the state of scrabble
+ * and contains the main line of execution
+ * @author Elyssa Grant, Gillian O'Connel, John Khalife, Sandy Alzabadani 
+ * @date 08/10/2024
+ */
+
+ public enum Words {
+    FIRST("First value"),
+    SECOND("Second value"),
+    THIRD("Third value");
+
+    private final String word;
+
+    // Constructor to associate the string with the enum value
+    Words(String word) {
+        this.word = word;
+    }
+
+public class Game {
+
+    
+
+}
