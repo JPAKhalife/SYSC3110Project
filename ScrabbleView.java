dc06171f4442a303eca581e277a8be7c68779d91
gillianoconnell <gillianem.oconnell@gmail.com>
2024-11-05 16:48:30 -0500

    Complete GUI, no controller methods

--- ScrabbleView.java
+++ ScrabbleView.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.Color;
 
 
 public class ScrabbleView extends JFrame implements GameObserver {
@@ -10,14 +11,17 @@ public class ScrabbleView extends JFrame implements GameObserver {
     JTextPane scorePane;
     Game game;
     Container turnElements;
+    Color TILE_COLOUR = new Color(240, 215, 149);
+    Color BOARD_COLOUR = new Color(103, 128, 78);
 
     public ScrabbleView(){
         //Configure frame
         super("Scrabble");
-        Game game = new Game();
+        game = new Game();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800,800);
         this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //Create GUI elements in frame
         turnElements = new Container(); //holds the current player's rack and the turn buttons
@@ -32,31 +36,35 @@ public class ScrabbleView extends JFrame implements GameObserver {
         GameController gameController = new GameController(game);
 
         //create board buttons
-        //char rowChar = 'a';
+        char rowChar = 'a';
+        JPanel boardPanel = new JPanel(new GridLayout(Board.BOARD_SIZE,Board.BOARD_SIZE));
         for(int i = 0; i < Board.BOARD_SIZE; i++) {
-            for (int j = 0; i <= Board.BOARD_SIZE; i++) {
+            for (int j = 0; j < Board.BOARD_SIZE; j++) {
                 boardButtons[i][j] = new JButton();
+                boardButtons[i][j].setFont(new Font(null, Font.BOLD, 14));
+                boardButtons[i][j].setBackground(BOARD_COLOUR);
                 boardButtons[i][j].addActionListener(gameController);
-                //String buttonCoordinate = Character.toString(rowChar) + Integer.toString(j);
-                String buttonCoordinate = Integer.toString(i) + "," + Integer.toString(j);
+                String buttonCoordinate = "board," + Character.toString(rowChar) + "," + Integer.toString(j);
                 boardButtons[i][j].setActionCommand(buttonCoordinate);
+                boardPanel.add(boardButtons[i][j]); //add to panel to be placed in frame
             }
-            //rowChar++;
+            rowChar++;
         }
 
-
-
         JPanel rackPanel = new JPanel(new GridLayout(1,7));
+        Player currentPlayer= game.getCurrentPlayer();
+        ArrayList<Letter> playerLetters = currentPlayer.getRack();
         for(int i = 0; i < 7; i++){
-            rackButtons[i] = new JButton(String.valueOf(LetterBag.getNextLetter().getLetter()));
+            rackButtons[i] = new JButton(Character.toString(playerLetters.get(i).getLetter()).toUpperCase());
+            rackButtons[i].setBackground(TILE_COLOUR);
+            rackButtons[i].setFont(new Font(null, Font.BOLD, 14));
             rackButtons[i].addActionListener(gameController);
-            rackButtons[i].setActionCommand("RACK_" + i);
+            rackButtons[i].setActionCommand("rack," + i);
             rackPanel.add(rackButtons[i]);
-
         }
 
         String[] commands = {"Submit", "Exchange","Skip"};
-        JPanel turnPanel = new JPanel(new GridLayout(1,3));
+        JPanel turnPanel = new JPanel(new GridLayout(3,1));
         for(int i = 0; i<3; i++){
             turnButtons[i] = new JButton(commands[i]);
             turnButtons[i].addActionListener(gameController);
@@ -65,21 +73,29 @@ public class ScrabbleView extends JFrame implements GameObserver {
         }
 
 
+        //Add rack and turn buttons below the board
+        JPanel playPanel = new JPanel(new GridLayout(1,2));
+        playPanel.add(rackPanel);
+        playPanel.add(turnPanel);
+        JPanel bottomDisplayPanel = new JPanel(new BorderLayout());
+        JTextPane currentPlayerField = new JTextPane();
+        currentPlayerField.setFont(new Font(null, Font.BOLD, 12));
+        currentPlayerField.setEditable(false);
+        currentPlayerField.setText("Player 1 Turn");
+        bottomDisplayPanel.add(currentPlayerField, BorderLayout.NORTH);
+        bottomDisplayPanel.add(playPanel, BorderLayout.CENTER);
 
-        JPanel statusPanel = new JPanel(new GridLayout(2,1));
+        //Create current player score pane
+        JTextPane scorePane = new JTextPane();
+        scorePane.setFont(new Font(null, Font.BOLD, 14));
         scorePane.setEditable(false);
-        currentPlayerPane.setEditable(false);
-        statusPanel.add(new JLabel("Score:"));
-        statusPanel.add(scorePane);
-        statusPanel.add(new JLabel("Turn:"));
-        statusPanel.add(currentPlayerPane);
+        scorePane.setText(currentScores());
 
-        this.add(statusPanel, BorderLayout.SOUTH);
-        this.add(rackPanel, BorderLayout.SOUTH);
-        this.add(turnPanel, BorderLayout.SOUTH);
+        this.add(bottomDisplayPanel, BorderLayout.SOUTH);
 
+        this.add(boardPanel, BorderLayout.CENTER);
 
-
+        this.add(scorePane, BorderLayout.EAST);
 
         this.setVisible(true);
     }
@@ -95,14 +111,15 @@ public class ScrabbleView extends JFrame implements GameObserver {
         }
     }
 
-    public void showScores(){
+    public String currentScores(){
         List<Player> players = game.getPlayers();
         String scores = "";
+        scores += "Current Scores\n---------------------\n";
         for(int i = 0; i < players.size(); i++){
             Player player = players.get(i);
-            scores = "Player" + String.valueOf(i)+ ":"+ player.getScore() + "\n";
+            scores += "Player " + String.valueOf(i + 1)+ " : "+ player.getScore() + "\n";
         }
-        scorePane.setText(scores);
+        return scores;
 
     }
 
