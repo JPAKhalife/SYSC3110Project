c069c0a6a013c95014dfb434b679aef8e95d1ab1
Elyss <elyssagrant@cmail.carleton.ca>
2024-11-14 08:53:51 -0500

    - Created static directional constants to explain what direction the word extends from - Began working on code to parse a valid word from the rack - Created a method to translate the indices into the String coordinates the rest of the code uses

--- AIPlayer.java
+++ AIPlayer.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class AIPlayer extends Player{
     private Board board;
+    public static final int NORTH = 0;
+    public static final int EAST = 1;
+    public static final int SOUTH = 2;
+    public static final int WEST = 3;
 
     /**
      * Default constructor for the AI player
@@ -34,11 +38,52 @@ public class AIPlayer extends Player{
      */
     private void decideWord()
     {
+        //Hashmap that stores the valid placement on the board and the valid directions
+        HashMap<String, int[]> validWordPlacements = new HashMap<>();
+        HashMap<String, ArrayList<String>> validWordsAtLocation = new HashMap<>();
+
         //Determine all the valid locations the AI can place a word and how large
+        for(int i = 0; i < Board.BOARD_SIZE; i++)
+        {
+            for(int j = 0; j < Board.BOARD_SIZE; j++)
+            {
+                int[] wordSizes = validWordSize(i, j);
+
+                //Ensuring that only locations with at least one free space are added
+                for(int k = 0; k < 4; k++)
+                {
+                    if(wordSizes[k] > 0)
+                    {
+                        String location = i + "," + j;
+                        validWordPlacements.put(location, wordSizes);
+                        validWordsAtLocation.put(location, new ArrayList<>());
+                        break;
+                    }
+                }
+            }
+        }
 
         //For each valid location/direction:
+        for(Iterator<String> iter = validWordPlacements.keySet().iterator(); iter.hasNext();)
+        {
+            String location = iter.next();
             //attempt to find a valid word that includes the valid location letter + letters on its rack
-                //Ensure the validity of its placement
+            for(int i = 0; i < 4; i++)
+            {
+                int sizeInDirection = validWordPlacements.get(location)[i];
+                if(sizeInDirection > 0)
+                {
+                    String[] indices = location.split(",");
+                    int j = Integer.parseInt(indices[0]);
+                    int k = Integer.parseInt(indices[1]);
+                    validWordsAtLocation.put(location, possibleWords(j, k, Math.min(sizeInDirection, 8)));
+                }
+            }
+        }
+
+        //Ensure the validity of its placement
+
+
 
     }
 
@@ -48,11 +93,11 @@ public class AIPlayer extends Player{
      * @param j the column of the board to check
      * @return An array of integers containing the number of letters that can be placed to each side of the inputted location. The directions start with north and go clockwise.
      */
-    private int[] validWordLocation(int i, int j)
+    private int[] validWordSize(int i, int j)
     {
         int[] freeSpacesInDirection = new int[4];
         Letter[][] boardAppearance = board.getBoardAppearance();
-        int offsetFromInput = 1;
+        int offsetFromInput = 0;
 
         //North
         if(boardAppearance[i - 1][j] == null)
@@ -63,7 +108,8 @@ public class AIPlayer extends Player{
                 offsetFromInput ++;
             }
 
-            freeSpacesInDirection[0] = offsetFromInput;
+            //We do not want accidental intersections, and thus leave one empty space alone
+            freeSpacesInDirection[NORTH] = offsetFromInput - 1;
         }
         //East
         if(boardAppearance[i][j + 1] == null)
@@ -75,7 +121,8 @@ public class AIPlayer extends Player{
                 offsetFromInput ++;
             }
 
-            freeSpacesInDirection[1] = offsetFromInput;
+            //We do not want accidental intersections, and thus leave one empty space alone
+            freeSpacesInDirection[EAST] = offsetFromInput - 1;
         }
         //This is one row down, aka South
         if(boardAppearance[i + 1][j] == null)
@@ -87,7 +134,8 @@ public class AIPlayer extends Player{
                 offsetFromInput ++;
             }
 
-            freeSpacesInDirection[2] = offsetFromInput;
+            //We do not want accidental intersections, and thus leave one empty space alone
+            freeSpacesInDirection[SOUTH] = offsetFromInput - 1;
         }
         //West
         if(boardAppearance[i][j - 1] == null)
@@ -99,7 +147,8 @@ public class AIPlayer extends Player{
                 offsetFromInput ++;
             }
 
-            freeSpacesInDirection[1] = offsetFromInput;
+            //We do not want accidental intersections, and thus leave one empty space alone
+            freeSpacesInDirection[WEST] = offsetFromInput - 1;
         }
 
         return freeSpacesInDirection;
@@ -109,6 +158,7 @@ public class AIPlayer extends Player{
      * This method returns all the possible words that can be made from a particular location
      * @param i The row of the location to be examined
      * @param j The column of the location to be examined
+     * @param max_size The maximum size the word can be to fit on the board
      * @return An arraylist that holds all the possible words that can be placed at one location on the board, sorted by score
      */
     public ArrayList<String> possibleWords(int i, int j, int max_size)
@@ -139,5 +189,18 @@ public class AIPlayer extends Player{
         }
 
         return validWords;
+    }
+
+    /**
+     * This method provides a simple way of translating the indices of the board into the scrabble notation that the Board class relies on
+     * @param i the row of the board
+     * @param j the column in the board
+     * @return A string representation of official scrabble notation for the inputted location
+     */
+    private String translateIndexToScrabbleNotation(int i, int j)
+    {
+        char rowChar = (char) ('a' + i);
+
+        return Character.toString(rowChar) + j;
     }
 }
